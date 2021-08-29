56689ed917d8f636c7bf4084492b33d97f6ef853
Erik Doernenburg <erik@doernenburg.com>
2021-08-29 16:47:31 +0200

    Removed unneeded code.

--- ScreenSaver/library/Util.swift
+++ ScreenSaver/library/Util.swift
@@ -20,51 +20,17 @@ class Util
 {
     class func randomBool() -> Bool
     {
-        return arc4random_uniform(2) == 0
+        arc4random_uniform(2) == 0
     }
     
     class func randomInt(_ max: Int) -> Int
     {
-        return Int(arc4random_uniform(UInt32(max)))
+        Int(arc4random_uniform(UInt32(max)))
     }
     
     class func randomInt(range: ClosedRange<Int>) -> Int
     {
-        return randomInt(range.upperBound - range.lowerBound + 1) - range.lowerBound
-    }
-
-    class func randomCGFloat(_ max: Int = 1) -> CGFloat
-    {
-        return CGFloat(randomInt(max * 100_000)) / CGFloat(100_000)
-    }
-    
-    class func randomDouble(_ max: Int = 1) -> Double
-    {
-        return Double(randomInt(max * 100_000)) / Double(100_000)
-    }
-
-    class func gaussian(_ x: Double, mean: Double, variance: Double) -> Double {
-        (1.0 / sqrt( 2 * Double.pi * variance)) * pow(M_E, -1 * pow(x - mean, 2.0) / (2 * variance))
-    }
-
-    class func sizeofArray<T>(_ array: [T]) -> Int
-    {
-        return array.count * MemoryLayout<T>.size
-    }
-
-    class func enumForCode<E: RawRepresentable>(_ code :Int, defaultCase: E) -> E where E.RawValue == Int
-    {
-        let val: Int
-        if code == -1 {
-            var maxValue: Int = 0
-            while let _ = E(rawValue: maxValue) {
-                maxValue += 1
-            }
-            val = Util.randomInt(maxValue)
-        } else {
-            val = code
-        }
-        return E(rawValue: val) ?? defaultCase
+        randomInt(range.upperBound - range.lowerBound + 1) - range.lowerBound
     }
     
 }
